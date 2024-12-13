55ecebafec6b6a40628d3378eee3cfd23c5af21b
Maxime GRECO <108299743+MaxGrc42@users.noreply.github.com>
2024-12-13 11:53:14 -0500

    test seo

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts">
+useHead({
+  // or as a function
+  meta: [
+    {
+      name: 'google-site-verification',
+      content: 'jbMawkt9ThQ5v6cnubQQi2tHenUDZw744yUku2mexrM'
+    }
+  ]
+})
+</script>
 <template>
-  <head>
-    <meta
-      name="google-site-verification"
-      content="jbMawkt9ThQ5v6cnubQQi2tHenUDZw744yUku2mexrM"
-    />
-  </head>
   <div>
     <NuxtPage />
   </div>
